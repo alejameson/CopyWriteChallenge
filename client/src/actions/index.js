6eedd6278fb1bc22530f6fa917ebd7353ef1cffd
Ale Jameson <alejameson01@gmail.com>
2022-07-28 22:27:06 -0300

    Fix localhost problem

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,7 @@ export const TEXT_TRANSFORM = "TEXT_TRANSFORM";
 
 export function getTextTransform(textTransform){
     return function(dispatch){
-        return axios.get(`/iecho?text=${textTransform}`)
+        return axios.get(`http://localhost:3001/iecho?text=${textTransform}`)
         .then((response) => {
             dispatch({
                 type: TEXT_TRANSFORM,
